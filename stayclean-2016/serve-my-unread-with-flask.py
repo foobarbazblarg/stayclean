eab2bb79f9850e07a1fce795bba6defb0cf1a196
foobarbazblarg <foobarbazblarg@gmail.com>
2016-12-05 21:46:54 -0600

    nightly commit

--- stayclean-2016/serve-my-unread-with-flask.py
+++ stayclean-2016/serve-my-unread-with-flask.py
@@ -25,6 +25,7 @@ sys.setdefaultencoding('utf8')
 
 # Edit me!
 activeCommentHashFiles = [ 'retiredcommenthashes.txt',
+                           '../stayclean-2016-december/retiredcommenthashes.txt',
                            '../stayclean-2016-november/retiredcommenthashes.txt',
                            '../stayclean-2016-october/retiredcommenthashes.txt',
                            '../stayclean-2016-september/retiredcommenthashes.txt',
