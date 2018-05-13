4c9f25a868758d439a741d86c4a17ee6cfbab365
foobarbazblarg <foobarbazblarg@gmail.com>
2018-05-13 17:03:01 -0500

    Nightly commit

--- installRequirements.sh
+++ installRequirements.sh
@@ -1,1 +1,2 @@
 sudo pip install -r requirements.txt
+sudo pip install --ignore-installed six
--- stayclean-2018/display.py
+++ stayclean-2018/display.py
@@ -8,7 +8,7 @@ import time
 import pyperclip
 
 # EDIT ME!
-currentMonthTotalDays = 28
+currentMonthTotalDays = 30
 year = 2018
 
 currentMonthIndex = datetime.date.today().month
